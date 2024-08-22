7dda54b6429eeab32e0db68e2e4685fd4244716e
TWL <2405077320@qq.com>
2024-08-22 16:46:09 +0800

    修改菜单

--- src/views/external/menu/menuOption.ts
+++ src/views/external/menu/menuOption.ts
@@ -1,7 +1,34 @@
 import axios from 'axios'
 import {AxiosResult} from '@utils/interface'
+import {MenuBean} from "../../theme/menu/menuModels";
 
+const getMenuById = (id: string) => {
+    return axios.get(`/menu/${id}`).then((res: { data: AxiosResult }) => {
+        return res.data
+    })
+}
+
+const createMenu = (menu: MenuBean) => {
+    return axios.post('/menu', menu).then((res: { data: AxiosResult }) => {
+        return res.data
+    })
+}
+
+const updateMenu = (menu: MenuBean) => {
+    return axios.put('/menu', menu).then((res: { data: AxiosResult }) => {
+        return res.data
+    })
+}
+
+const deleteMenuById = (id: string) => {
+    return axios.delete(`/menu/${id}`).then((res: { data: AxiosResult }) => {
+        return res.data
+    })
+}
 
 export {
-
+    getMenuById,
+    createMenu,
+    updateMenu,
+    deleteMenuById,
 }
